e6aa909b95e34f0dad21e05691154221714c763d
RH AGENCY <rhagency07@gmail.com>
2026-02-28 14:17:21 +0700

    Update App.tsx

--- ai-whiteboard-tutor/src/App.tsx
+++ ai-whiteboard-tutor/src/App.tsx
@@ -1,16 +1,5 @@
-import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Landing from "./pages/Landing";
-import CompatibilityCheck from "./pages/CompatibilityCheck";
 import WhiteboardLesson from "./pages/WhiteboardLesson";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/check" element={<CompatibilityCheck />} />
-      <Route path="/lesson" element={<WhiteboardLesson />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+  return <WhiteboardLesson />;
 }
